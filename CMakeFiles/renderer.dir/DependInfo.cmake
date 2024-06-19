4e8ddd38207f0760ad587afda334684e6b24c706
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-06-19 09:11:12 +0200

    define OMPI_SKIP_MPICXX

---
+++ CMakeFiles/renderer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lorenzo/Dev/parallel_raytracing/multi-core_MPI_OpenMP/src/Application.cpp" "CMakeFiles/renderer.dir/src/Application.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/Application.cpp.o.d"
+  "/home/lorenzo/Dev/parallel_raytracing/multi-core_MPI_OpenMP/src/Renderer.cpp" "CMakeFiles/renderer.dir/src/Renderer.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/Renderer.cpp.o.d"
+  "/home/lorenzo/Dev/parallel_raytracing/multi-core_MPI_OpenMP/src/Scene.cpp" "CMakeFiles/renderer.dir/src/Scene.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/Scene.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
